f8a7c5a52cb18e031c232a8ba0b43cfdeca64e61
Chris Gonzalez <christogonz92@gmail.com>
2025-06-30 17:17:31 +0200

    Initial Commit

---
+++ SaveCraft/SaveCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveCraftApp.swift
+//  SaveCraft
+//
+//  Created by Christopher Gonzalez on 2025-06-30.
+//
+
+import SwiftUI
+
+@main
+struct SaveCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
